827c592c5d55f24f17e078575d45c91b5f3cd998
MoveMaNow <nut647864@gmail.com>
2026-05-10 19:51:38 +0700

    add javascript

--- Webminiproject/HTML/JavaScript/login.js
+++ Webminiproject/HTML/JavaScript/login.js
@@ -2,14 +2,19 @@ const form = document.getElementById("login-form");
 
 form.addEventListener("submit", function(event){
 
+    event.preventDefault();
+
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
     if(username === "" || password === "") {
 
-        event.preventDefault();
-
         alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+        return;
+    } else {
+
+        window.location.href = "index.html";
+
     }
 
 });
